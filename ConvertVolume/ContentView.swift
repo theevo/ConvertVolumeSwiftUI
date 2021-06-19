991d10301291a4340a468bab49ee42c71be64d2c
Theo Vora <theo.vora@gmail.com>
2021-06-18 19:40:40 -0600

    Complete design structure

--- ConvertVolume/ContentView.swift
+++ ConvertVolume/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fromUnit = 1
+    @State private var toUnit = 0
+    @State private var fromQuantity = "1"
+    @State private var toQuantity = "1000"
+    
+    let units = ["milliliters", "liters", "cups", "pints", "gallons"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section(header: Text("Take this")) {
+                TextField("Quantity", text: $fromQuantity)
+                Picker("Unit", selection: $fromUnit) {
+                    ForEach(0 ..< units.count) {
+                        Text("\(units[$0])")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            Section(header: Text("Convert to")) {
+                Picker("Unit", selection: $toUnit) {
+                    ForEach(0 ..< units.count) {
+                        Text("\(units[$0])")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+                Text("\(toQuantity)")
+                
+            }
+        }
     }
 }
 
