b2d5e3b91b835cd0bbb54bddff8266190dd68597
Theo Vora <theo.vora@gmail.com>
2021-06-18 19:16:37 -0600

    Initial Commit

---
+++ ConvertVolume/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConvertVolume
+//
+//  Created by Theo Vora on 6/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ConvertVolume/ConvertVolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertVolumeApp.swift
+//  ConvertVolume
+//
+//  Created by Theo Vora on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ConvertVolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
