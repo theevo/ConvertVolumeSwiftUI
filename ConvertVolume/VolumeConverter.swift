e4487c4144adec03c97c7e49929dded372a0a543
Theo Vora <theo.vora@gmail.com>
2021-06-18 20:23:25 -0600

    Add VolumeConverter

---
+++ ConvertVolume/VolumeConverter.swift
@@ -0,0 +1,32 @@
+//
+//  VolumeConverter.swift
+//  ConvertVolume
+//
+//  Created by Theo Vora on 6/18/21.
+//
+
+import Foundation
+
+let VolumeDictionary: [String: UnitVolume] = [
+    "milliliters": UnitVolume.milliliters,
+    "liters": UnitVolume.liters,
+    "cups": UnitVolume.cups,
+    "pints": UnitVolume.pints,
+    "gallons": UnitVolume.gallons
+]
+
+struct VolumeConverter {
+    let inputQuantity: Double
+    let inputUnit: UnitVolume
+    let outputUnit: UnitVolume
+    
+    init?(inputQuantity: String, inUnit: String, outUnit: String) {
+        guard let inputQuantity = Double(inputQuantity),
+              let inputUnit = VolumeDictionary[inUnit],
+              let outputUnit = VolumeDictionary[outUnit] else { return nil }
+        
+        self.inputQuantity = inputQuantity
+        self.inputUnit = inputUnit
+        self.outputUnit = outputUnit
+    }
+}
